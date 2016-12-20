e41bb75a5dcec0a3a9e7e273ffa0895dd2aab4cd
LEE SEUNG CHAN <LEE SEUNG CHAN@DESKTOP-SBKPE9B>
2016-12-20 12:22:21 +0900

    git_test

--- src/com/javalec/accesstest/MainClass.java
+++ src/com/javalec/accesstest/MainClass.java
@@ -2,13 +2,11 @@ package com.javalec.accesstest;
 
 public class MainClass {
 	public static void main(String[] args) {
-		AccessTest accesstest= new AccessTest();
-		
-		//at.a; priavate때문에 접근이 안됨
-		
+		AccessTest accesstest = new AccessTest();
+
+		// at.a; priavate때문에 접근이 안됨
+
 		accesstest.meth();
-		
-		
 
 	}
 }
