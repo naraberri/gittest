cf49cb95bcc3bdb8a87152d23fd1e933ffeff641
LEE SEUNG CHAN <LEE SEUNG CHAN@DESKTOP-SBKPE9B>
2016-12-21 09:58:54 +0900

    super

--- src/com/javalec/superex/MainClass.java
+++ src/com/javalec/superex/MainClass.java
@@ -3,10 +3,9 @@ package com.javalec.superex;
 public class MainClass {
 
 	public static void main(String[] args) {
-			
+
 		ChildClass childClass = new ChildClass();
 		childClass.method1();
 	}
-	
+
 }
- 
