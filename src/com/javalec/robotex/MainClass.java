4ebaf40b129511b4663dde246e06e4b1a23a3b9d
LEE SEUNG CHAN <LEE SEUNG CHAN@DESKTOP-SBKPE9B>
2016-12-26 10:12:49 +0900

    test

---
+++ src/com/javalec/robotex/MainClass.java
@@ -0,0 +1,12 @@
+package com.javalec.robotex;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		SuperRobot robot = new SuperRobot();
+		 
+		
+	}
+
+}
---
+++ src/com/javalec/robotex/SuperRobot.java
@@ -0,0 +1,33 @@
+package com.javalec.robotex;
+
+public class SuperRobot {
+
+	public SuperRobot() {
+		
+	}
+	
+	public void actionWalk(){
+		System.out.println("걸을 수 있습니다.");
+	}
+	
+	public void actionRun(){
+		System.out.println("달릴 수 있습니다.");
+	}
+	
+	public void actionFly(){
+		System.out.println("날 수 있습니다.");
+	}
+	
+	public void actionMisail(){
+		System.out.println("미사일 쏠 수 있습니다.");
+	}
+	
+	public void actionKnife(){
+		System.out.println("레이저 검이 있습니다.");
+	}
+	
+	public void shape(){
+		System.out.println("SuperRobot 로봇입니다. 외형은 팔, 다리, 몸통, 머리가 있습니다.");
+	}
+	
+}
